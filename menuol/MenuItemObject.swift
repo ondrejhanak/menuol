3b13ec25f8cb457817c061299ff789c18807fdc5
Ondrej Hanak <oh@ondrejhanak.cz>
2017-07-19 13:30:58 +0200

    MeuItemObject

---
+++ menuol/MenuItemObject.swift
@@ -0,0 +1,34 @@
+//
+//  MenuItemObject.swift
+//  menuol
+//
+//  Created by Ondrej Hanak on 19/07/2017.
+//  Copyright © 2017 Ondrej Hanak. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class MenuItemObject: Object {
+
+	// MARK: - Persistent properties
+
+	dynamic var title = ""
+	dynamic var day = 0
+	dynamic var month = 0
+	dynamic var year = 0
+	dynamic var order = 0
+	dynamic var orderDescription = ""
+	dynamic var priceDescription = ""
+
+	// MARK: - Meta
+
+	override static func primaryKey() -> String? {
+		return nil
+	}
+
+	override static func indexedProperties() -> [String] {
+		return ["order", "year", "month", "day"]
+	}
+
+}
