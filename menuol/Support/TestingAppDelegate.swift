f83bbd0753daf70599d17f4ec4465c308fc5309f
Ondrej Hanak <oh@ondrejhanak.cz>
2018-08-22 13:45:25 +0200

    TestingAppDelegate

---
+++ menuol/Support/TestingAppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  menuol
+//
+//  Created by Ondrej Hanak on 22/08/2018.
+//  Copyright © 2018 Ondrej Hanak. All rights reserved.
+//
+
+import UIKit
+
+final class TestingAppDelegate: NSObject, UIApplicationDelegate {
+
+	// MARK: - Lifecycle
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+
+}
---
+++ menuol/Support/main.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  menuol
+//
+//  Created by Ondrej Hanak on 22/08/2018.
+//  Copyright © 2018 Ondrej Hanak. All rights reserved.
+//
+
+import UIKit
+
+let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
+let delegateClass: AnyClass =	NSClassFromString("menuol_unit_tests.TestingAppDelegate") ?? AppDelegate.self
+let delegateClassString = NSStringFromClass(delegateClass)
+UIApplicationMain(CommandLine.argc, args, nil, delegateClassString)
