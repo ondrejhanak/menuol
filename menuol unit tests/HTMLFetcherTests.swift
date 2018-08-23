46bcdd69899a80383e3b30a813eb9011f84798ec
Ondrej Hanak <oh@ondrejhanak.cz>
2018-08-23 13:26:33 +0200

    Fetcher tests

--- menuol unit tests/HTMLFetcherTests.swift
+++ menuol unit tests/HTMLFetcherTests.swift
@@ -13,31 +13,44 @@ let kNetworkTimeout = 10.0
 
 final class HTMLFetcherTests: XCTestCase {
 
-	func testFetchHTMLSuccess() {
-		let expectation = XCTestExpectation(description: "HTML fetching")
-		let fetcher = HTMLFetcher()
-		let url = URL(string: "https://example.org")!
-		fetcher.fetchHTML(url: url) { result in
-			switch result {
-			case .success:
-				expectation.fulfill()
-			case .failure:
-				XCTFail("HTML fetching unexpectedly failed.")
-			}
-		}
-		self.wait(for: [expectation], timeout: kNetworkTimeout)
-	}
-
 	func testFetchHTMLFailure() {
 		let expectation = XCTestExpectation(description: "HTML fetching")
 		let fetcher = HTMLFetcher()
-		let url = URL(string: "https://asdfasdf.asdfasdfasd.fasdfasdf.com")!
+		let url = URL(string: "https://some.hopefully.non.existing.url.com")!
 		fetcher.fetchHTML(url: url) { result in
 			switch result {
 			case .success:
 				XCTFail("HTML fetching unexpectedly succeeded.")
 			case .failure:
 				expectation.fulfill()
+			}
+		}
+		self.wait(for: [expectation], timeout: kNetworkTimeout)
+	}
+
+	func testFetchVenueHTML() {
+		let expectation = XCTestExpectation(description: "Fetching venue HTML")
+		let fetcher = HTMLFetcher()
+		fetcher.fetchVenueHTML(for: Date()) { (result) in
+			switch result {
+			case .success:
+				expectation.fulfill()
+			case .failure:
+				XCTFail("Venue HTML fetching unexpectedly failed.")
+			}
+		}
+		self.wait(for: [expectation], timeout: kNetworkTimeout)
+	}
+
+	func testMenuVenueHTML() {
+		let expectation = XCTestExpectation(description: "Fetching menu HTML")
+		let fetcher = HTMLFetcher()
+		fetcher.fetchMenuHTML(slug: "JAZZ-TIBET-CLUB-id38") { (result) in
+			switch result {
+			case .success:
+				expectation.fulfill()
+			case .failure:
+				XCTFail("Menu HTML fetching unexpectedly failed.")
 			}
 		}
 		self.wait(for: [expectation], timeout: kNetworkTimeout)
