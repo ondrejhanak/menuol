7c351baab44177e9a41bf8ae806b077566cf4b59
Ondrej Hanak <oh@ondrejhanak.cz>
2018-08-24 10:22:36 +0200

    AppDelegate tests

---
+++ menuol unit tests/AppDelegateTests.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegateTests.swift
+//  menuol unit tests
+//
+//  Created by Ondřej Hanák on 23. 08. 2018.
+//  Copyright © 2018 Ondrej Hanak. All rights reserved.
+//
+
+import XCTest
+@testable import menuol
+
+final class AppDelegateTests: XCTestCase {
+
+	func testDelegate() {
+		let application = UIApplication.shared
+		let delegate = AppDelegate()
+		let result = delegate.application(application, didFinishLaunchingWithOptions: nil)
+		XCTAssert(result == true)
+		XCTAssert(delegate.window?.rootViewController != nil)
+	}
+
+}
