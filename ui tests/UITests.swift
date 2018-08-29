6b24b11e8b75177ce630a5aba6fd432d51a72606
Ondrej Hanak <oh@ondrejhanak.cz>
2018-08-29 09:05:52 +0200

    UI tests bundle

---
+++ ui tests/UITests.swift
@@ -0,0 +1,13 @@
+//
+//  UITests.swift
+//  ui tests
+//
+//  Created by Ondřej Hanák on 29. 08. 2018.
+//  Copyright © 2018 Ondrej Hanak. All rights reserved.
+//
+
+import XCTest
+
+final class UITests: XCTestCase {
+    
+}
